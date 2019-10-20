cab189b5424ed90bbca97f21eaeca1e371fd926b
Chris Tsang <tyt2y7@gmail.com>
2019-10-21 02:48:03 +0800

    Generate mesh

---
+++ Assets/Demo/Demo.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Demo : MonoBehaviour {
+
+    private void Start() {
+        Mesh mesh = new Mesh();
+        List<Vector3> positions = new List<Vector3>();
+        List<Vector4> texCoord0 = new List<Vector4>();
+        List<Color> colors = new List<Color>();
+        List<int> indices = new List<int>();
+
+        positions.Add(new Vector3(-1, 0, 1));
+        positions.Add(new Vector3(1, 0, 1));
+        positions.Add(new Vector3(-1, 0, -1));
+        positions.Add(new Vector3(1, 0, -1));
+
+        colors.Add(Color.red);
+        colors.Add(Color.green);
+        colors.Add(Color.blue);
+        colors.Add(Color.yellow);
+
+        texCoord0.Add(new Vector4(0, 1, 0, 0));
+        texCoord0.Add(new Vector4(1, 1, 0, 0));
+        texCoord0.Add(new Vector4(1, 0, 0, 0));
+        texCoord0.Add(new Vector4(0, 0, 0, 0));
+
+        indices.Add(0);
+        indices.Add(1);
+        indices.Add(2);
+        indices.Add(2);
+        indices.Add(1);
+        indices.Add(3);
+
+        mesh.SetVertices(positions);                      // set our position buffer
+        mesh.SetUVs(0, texCoord0);                        // set a tex coord buffer (not sure if you'll need this or not)
+        mesh.SetColors(colors);                           // set our color buffer
+        mesh.SetTriangles(indices, 0);                    // set our index buffer
+
+        GameObject gameObject = new GameObject("Mesh", typeof(MeshFilter), typeof(MeshRenderer));
+        gameObject.transform.localScale = new Vector3(1,1,1);
+        gameObject.GetComponent<MeshFilter>().mesh = mesh;
+        gameObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Vase/Rectangle");
+    }
+}
---
+++ Assets/Resources/Vase/Rectangle.shader
@@ -0,0 +1,41 @@
+﻿Shader "Rectangle" {
+
+    SubShader {
+        //the material is completely non-transparent and is rendered at the same time as the other opaque geometry
+        Tags { "RenderType"="Opaque" "Queue"="Geometry" }
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            
+            #include "UnityCG.cginc"
+
+            struct appdata {
+                float4 vertex : POSITION;
+                float4 uv : TEXCOORD0;
+                fixed4 color : COLOR;
+            };
+
+            struct v2f {
+                float4 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+                fixed4 color : COLOR;
+            };
+
+            v2f vert (appdata v) {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                o.color = v.color;
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target {
+                return i.color;
+            }
+            ENDCG
+        }
+    }
+}
