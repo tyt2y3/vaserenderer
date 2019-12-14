10b265ab82fd26f76074d41061f50ea2c1032319
Chris Tsang <tyt2y7@gmail.com>
2019-12-15 03:35:35 +0800

    Polybezier demo

--- Assets/Demo/Demo.cs
+++ Assets/Demo/Demo.cs
@@ -7,7 +7,7 @@ public class Demo : MonoBehaviour {
     private void Start() {
 
         Polyline.Opt opt = new Polyline.Opt();
-        opt.feather = false;
+        opt.feather = true;
         opt.feathering = 15.0f;
         opt.joint = Polyline.Opt.PLJ_round;
         opt.cap = Polyline.Opt.PLC_round;
@@ -19,24 +19,51 @@ public class Demo : MonoBehaviour {
             opt.world_to_screen_ratio = Vector3.Distance(a,b) / new Vector3(1,1,0).magnitude;
         }
 
-        Polyline polyline = new Polyline(
-            new List<Point> {
-                new Point(0, .75f),
-                new Point(-.75f, -.1f),
-                new Point(.75f, .1f),
-                new Point(0, -.75f),
-            },
-            new List<Color> {
-                Color.red,
-                Color.green,
-                Color.blue,
-                Color.red,
-            },
-            new List<float> {
-                0.05f, 0.15f, 0.15f, 0.05f,
-            },
-            opt
-        );
+        int mode = 2;
+        Polyline polyline = null;
+
+        if (mode == 1) {
+            polyline = new Polyline(
+                new List<Point> {
+                    new Point(0, .75f),
+                    new Point(-.75f, -.1f),
+                    new Point(.75f, .1f),
+                    new Point(0, -.75f),
+                },
+                new List<Color> {
+                    Color.red,
+                    Color.green,
+                    Color.blue,
+                    Color.red,
+                },
+                new List<float> {
+                    0.05f, 0.15f, 0.15f, 0.05f,
+                },
+                opt
+            );
+        } else if (mode == 2) {
+            Polybezier polybezier = new Polybezier(
+                new List<Point> {
+                    new Point(0, .75f),
+                    new Point(-.75f, -.1f),
+                    new Point(.75f, .1f),
+                    new Point(0, -.75f),
+                },
+                new Vaser.Gradient (
+                    new List<Vaser.Gradient.Stop> {
+                        new Vaser.Gradient.Stop(0.0f,  Color.red),
+                        new Vaser.Gradient.Stop(0.0f,  0.1f),
+                        new Vaser.Gradient.Stop(0.25f, Color.green),
+                        new Vaser.Gradient.Stop(0.5f,  Color.blue),
+                        new Vaser.Gradient.Stop(1.0f,  Color.red),
+                    }
+                ),
+                new Polybezier.Opt {
+                    world_to_screen_ratio = opt.world_to_screen_ratio
+                }
+            );
+            polyline = polybezier.Render(opt);
+        }
 
         Mesh mesh = polyline.GetMesh();
 
